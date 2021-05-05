a1a2ccdef73f960b44f8f0b658148bb96b72890f
RameshZS <83690680+RameshZS@users.noreply.github.com>
2021-05-05 11:52:01 +0530

    Update recipe-details.js

--- app/models/recipe-details.js
+++ app/models/recipe-details.js
@@ -1,7 +1,6 @@
-import Model, { attr,belongsTo } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class RecipeDetailsModel extends Model {
-  @belongsTo('user-details') userName;
 
   @attr recipe_name;
   @attr time;
